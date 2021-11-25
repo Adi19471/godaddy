bee7316c3e07d60e0402de187cbf157dc6e06e8e
ADI <akumatha@gmail.com>
2021-11-25 13:56:29 +0530

    yes

--- GODADDY_ASSIGNMENT/urls.py
+++ GODADDY_ASSIGNMENT/urls.py
@@ -21,9 +21,8 @@ from django.conf import  settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('assignmntAPP.urls'))
+    path('',include('assignmntAPP.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns  += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
